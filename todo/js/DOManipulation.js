68162559d4344a0447cc2b270165a13ec8c13029
george najm <georgenajm28@yahoo.com>
2017-11-07 19:51:55 -0500

    added hidden input

--- todo/js/DOManipulation.js
+++ todo/js/DOManipulation.js
@@ -1,1 +1,14 @@
-$l("div").attr("done","asd");
+$l(".addtodo-button").on("click", () => {
+  const inp = $l(".hidden")
+  inp.removeClass("hidden")
+  inp.addClass("shown");
+  const newTodo = $l("<li>");
+  newTodo.addClass("todo-item");
+  const todoList = $l(".todo-list");
+  todoList.append(newTodo);
+});
+
+$l(".add-todo").on("click", () => {
+  const inp = $l("#todo-title");
+  const title = inp.array[0].value;
+});
