816fd2d8b666e1aadf59de46f009c89917a53aa2
george najm <georgenajm28@yahoo.com>
2017-11-07 20:36:21 -0500

    add todos

--- todo/js/DOManipulation.js
+++ todo/js/DOManipulation.js
@@ -2,13 +2,22 @@ $l(".addtodo-button").on("click", () => {
   const inp = $l(".hidden")
   inp.removeClass("hidden")
   inp.addClass("shown");
-  const newTodo = $l("<li>");
-  newTodo.addClass("todo-item");
-  const todoList = $l(".todo-list");
-  todoList.append(newTodo);
 });
 
 $l(".add-todo").on("click", () => {
   const inp = $l("#todo-title");
   const title = inp.array[0].value;
+  const todoList = $l(".todo-list");
+  const newTodo = $l("<li>");
+  const todoTitle = $l("<h1>");
+  todoTitle.addClass("todo-title");
+  todoTitle.append(`${title}`);
+  newTodo.append(todoTitle);
+  const subButton = $l("<button>");
+  subButton.addClass("subtask-button");
+  subButton.append("SUBTASKS");
+  newTodo.append(subButton);
+  newTodo.addClass("todo-item");
+  todoList.append(newTodo);
+
 });
