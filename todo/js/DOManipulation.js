55c743366e44e758e4d3ea6abc44f14c8e164e38
george najm <georgenajm28@yahoo.com>
2017-11-16 17:25:29 -0500

    fix eventlistener bug

--- todo/js/DOManipulation.js
+++ todo/js/DOManipulation.js
@@ -1,45 +1,45 @@
-let subtasksArray =
-{
-  0:['Add Documentation','Contact info','Format entry'],
-  1:['Talk to VPR', 'Skype Japan'],
-  2:['Tomatos', 'Fruitzz']
-};
-let numTasks = $d('.todo-item').array.length
+$d(()=>{
 
 
-$d(".addtodo-button").on("click", () => {
-  const addContent = $d(".add-div-content")
-  addContent.addClass("shown");
-});
-
-$d(".add-todo").on("click", () => {
-  const inp = $d("#todo-title");
-  const title = inp.array[0].value;
-  const todoList = $d(".todo-list");
-  const newTodo = $d("<li>");
-  const todoTitle = $d("<h1>");
-  todoTitle.addClass("todo-title");
-  todoTitle.append(`${title}`);
-  newTodo.append(todoTitle);
-  const subButton = $d("<button>");
-  subButton.addClass("subtask-button");
-  subButton.append("SUBTASKS");
-  newTodo.append(subButton);
-  newTodo.addClass("todo-item");
-  todoList.append(newTodo);
-  const addContent = $d(".add-div-content")
-  addContent.removeClass("hidden");
-  addContent.removeClass("shown");
-});
-
-$d(".cleartodo-button").on("click", () => {
-  $d('.todo-list').empty();
-  $d('.subtasks').empty();
-});
+  let subtasksArray =
+  {
+    0:['Add Documentation','Contact info','Format entry'],
+    1:['Talk to VPR', 'Skype Japan'],
+    2:['Tomatos', 'Fruitzz']
+  };
+  let numTasks = $d('.todo-item').array.length
 
 
-$d(".subtask-button").on("click", (e) => {
-  debugger
+  $d(".addtodo-button").on("click", () => {
+    const addContent = $d(".add-div-content")
+    addContent.addClass("shown");
+  });
+
+  $d(".add-todo").on("click", () => {
+    const inp = $d("#todo-title");
+    const title = inp.array[0].value;
+    const todoList = $d(".todo-list");
+    const newTodo = $d("<li>");
+    const todoTitle = $d("<h1>");
+    todoTitle.addClass("todo-title");
+    todoTitle.append(`${title}`);
+    newTodo.append(todoTitle);
+    const subButton = $d("<button>");
+    subButton.addClass("subtask-button");
+    subButton.append("SUBTASKS");
+    newTodo.append(subButton);
+    newTodo.addClass("todo-item");
+    todoList.append(newTodo);
+    const addContent = $d(".add-div-content")
+    addContent.removeClass("shown");
+  });
+
+  $d(".cleartodo-button").on("click", () => {
+    $d('.todo-list').empty();
+    $d('.subtasks').empty();
+  });
+
+$d('.todo-list').on('click' , (e) => {
   let idx = parseInt($d(e.target).attr('key'));
   const substasks = $d('.subtasks');
   substasks.empty();
@@ -54,5 +54,18 @@ $d(".subtask-button").on("click", (e) => {
 });
 
 
-
-const subtasks = [];
+  // $d(".subtask-button").on("click", (e) => {
+  //   debugger
+  //   let idx = parseInt($d(e.target).attr('key'));
+  //   const substasks = $d('.subtasks');
+  //   substasks.empty();
+  //   const subtasksUl = $d('<ul>');
+  //   subtasksUl.addClass('subtasks-ul');
+  //   subtasksArray[idx].forEach((subt) => {
+  //     const subLi = $d('<li>');
+  //     subLi.append(subt);
+  //     subtasksUl.append(subLi);
+  //   });
+  //   substasks.append(subtasksUl);
+  // });
+});
