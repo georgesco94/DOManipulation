85324465f14fe5b669e7f69b4f2075fde53bccbf
george najm <georgenajm28@yahoo.com>
2017-11-16 12:06:10 -0500

    changed name of function

--- todo/js/DOManipulation.js
+++ todo/js/DOManipulation.js
@@ -1,19 +1,19 @@
-$l(".addtodo-button").on("click", () => {
-  const inp = $l(".hidden")
+$d(".addtodo-button").on("click", () => {
+  const inp = $d(".hidden")
   inp.removeClass("hidden")
   inp.addClass("shown");
 });
 
-$l(".add-todo").on("click", () => {
-  const inp = $l("#todo-title");
+$d(".add-todo").on("click", () => {
+  const inp = $d("#todo-title");
   const title = inp.array[0].value;
-  const todoList = $l(".todo-list");
-  const newTodo = $l("<li>");
-  const todoTitle = $l("<h1>");
+  const todoList = $d(".todo-list");
+  const newTodo = $d("<li>");
+  const todoTitle = $d("<h1>");
   todoTitle.addClass("todo-title");
   todoTitle.append(`${title}`);
   newTodo.append(todoTitle);
-  const subButton = $l("<button>");
+  const subButton = $d("<button>");
   subButton.addClass("subtask-button");
   subButton.append("SUBTASKS");
   newTodo.append(subButton);
