82babc4927980804016dad1dcd59502880eec459
george najm <georgenajm28@yahoo.com>
2017-11-16 16:51:03 -0500

    added subtask button

--- todo/js/DOManipulation.js
+++ todo/js/DOManipulation.js
@@ -1,7 +1,15 @@
+let subtasksArray =
+{
+  0:['Add Documentation','Contact info','Format entry'],
+  1:['Talk to VPR', 'Skype Japan'],
+  2:['Tomatos', 'Fruitzz']
+};
+let numTasks = $d('.todo-item').array.length
+
+
 $d(".addtodo-button").on("click", () => {
-  const inp = $d(".hidden")
-  inp.removeClass("hidden")
-  inp.addClass("shown");
+  const addContent = $d(".add-div-content")
+  addContent.addClass("shown");
 });
 
 $d(".add-todo").on("click", () => {
@@ -19,9 +27,32 @@ $d(".add-todo").on("click", () => {
   newTodo.append(subButton);
   newTodo.addClass("todo-item");
   todoList.append(newTodo);
+  const addContent = $d(".add-div-content")
+  addContent.removeClass("hidden");
+  addContent.removeClass("shown");
 });
 
 $d(".cleartodo-button").on("click", () => {
-  debugger
   $d('.todo-list').empty();
+  $d('.subtasks').empty();
 });
+
+
+$d(".subtask-button").on("click", (e) => {
+  debugger
+  let idx = parseInt($d(e.target).attr('key'));
+  const substasks = $d('.subtasks');
+  substasks.empty();
+  const subtasksUl = $d('<ul>');
+  subtasksUl.addClass('subtasks-ul');
+  subtasksArray[idx].forEach((subt) => {
+    const subLi = $d('<li>');
+    subLi.append(subt);
+    subtasksUl.append(subLi);
+  });
+  substasks.append(subtasksUl);
+});
+
+
+
+const subtasks = [];
